4200b159b5573b9c6db126ba970982a67b541ee6
Manuel Danner <manuel.danner.aut@gmail.com>
2021-08-01 12:16:09 +0200

    rewrite code in python3

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pandas as pd
+import locale
 import time
-import array
 from enum import Enum
 import os.path
 
+
 class Coin(Enum):
-    Bitcoin = 0
-    Litecoin = 1
-    Bytecoin = 2
-    ReddCoin = 3
-    Vertcoin = 4
-    Dogecoin = 5
+    BTCBitcoin = 0
+    LTCLitecoin = 1
+    ETHEthereum = 2
+    DOGEDogecoin = 3
 
 
 def load_coin_value(file_name):
-    '''file_name  of type String'''
+    """
+    load the starting value of a coin for a given file_name
+    """
     try:
         with open(file_name + 'spy.txt', 'r') as f:
             file_content = f.readline()
@@ -26,7 +27,9 @@ def load_coin_value(file_name):
 
 
 def save_file(file_name, file_content):
-    '''file_name and file_content of type String'''
+    """
+    save file_content as coinename + spy.txt
+    """
     try:
         with open(file_name + 'spy.txt', 'a') as f:
             f.write(file_content)
@@ -35,38 +38,26 @@ def save_file(file_name, file_content):
         return False
 
 
-starttime = time.time()
-y = 1
-startvalue = array.array('d', (0 for i in range(0, len(Coin))))
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+buy_value = dict()
+
+for coin in Coin:
+    try:
+        if os.path.getsize(f"{coin.name}spy.txt") != 0:
+            buy_value[coin.name] = float(load_coin_value(coin.name))
+    except FileNotFoundError:
+        continue
+
 while True:
-    calls_df, = pd.read_html("https://coinmarketcap.com/all/views/all/", index_col=1, header=0)
+    calls_df = pd.read_html("https://coinmarketcap.com/all/views/all/", index_col=1, header=0)
+    now = time.strftime('%d/%m/%Y %H:%M:%S')
     for coin in Coin:
-        if not os.path.isfile(coin.name + 'spy.txt'):
-            with open(coin.name + 'spy.txt', 'w'):
-                pass
-        if os.stat(coin.name + 'spy.txt').st_size == 0:
-            x = calls_df.loc[coin.name]
-            now = time.strftime('%d/%m/%Y %H:%M:%S')
-            coinvalue = x[3]
-            coinvalue = float(coinvalue[1:])
-            startvalue[coin.value] = coinvalue
-            coinchange = round((coinvalue / startvalue[coin.value]), 4) * 100
-            coininfo = "$" + str(coinvalue) + " %-Value: " + str(coinchange) + " " + now
-            save_file(coin.name, coininfo)
-        else:
-            if y == 1:
-                startvalue[coin.value] = float(load_coin_value(coin.name))
-            x = calls_df.loc[coin.name]
-            now = time.strftime('%d/%m/%Y %H:%M:%S')
-            coinvalue = x[3]
-            coinvalue = float(coinvalue[1:])
-            coinchange = round((coinvalue / startvalue[coin.value]), 4) * 100
-            coininfo = "\n" + "$" + str(coinvalue) + " %-Value: " + str(coinchange) + " " + now
-            save_file(coin.name, coininfo)
-    print "tick " + str(y) + "\n"
-    y = y + 1
-    time.sleep(300.0 - ((time.time() - starttime) % 300.0))
+        coin_info = calls_df[2].loc[coin.name]
+        coin_price = float(locale.atof(coin_info.Price[1:]))
+        buy_value[coin.name] = buy_value.get(coin.name, coin_price)
+        coin_change = coin_price / buy_value[coin.name] * 100
+        coin_txt = f"${coin_price} %-Value: {coin_change:.2f} {now} \n"
+        save_file(coin.name, coin_txt)
 
-
-
-
+    time.sleep(300)
